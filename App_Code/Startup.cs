afa82c6cda3746cad342e00a83c7bca5f794180a
Julien Oudot <juoudot@microsoft.com>
2017-06-12 10:12:18 -0400

    initialize repository

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureAppService.Startup))]
+namespace AzureAppService
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
